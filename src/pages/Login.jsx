98120e4be6bda091e36229d065f7f94d50cc6709
LuisAxelMarinGarcia <213467@ids.upchiapas.edu.mx>
2023-03-25 16:43:29 -0600

    Front actualizado

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/login.css';
 import { Link } from 'react-router-dom';
+import io from "socket.io-client";
+
+const socket = io("http://localhost:3000/login");
+socket.on("login", (data) => {
+  console.log("Recibo todos los datos que son: ");
+  console.log(data)
+});;
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  }
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    var raw = JSON.stringify({
+      "email": email,
+      "password": password
+    });
+
+    console.log(email, password)
+    console.log(raw)
+    fetch('http://localhost:3000/user/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: raw,
+      redirect: 'follow'
+    })
+      .catch(error => console.error(error));
+  }
+
   return (
-    <div class="register-container">
-      <form className="register-form">
+    <div className="register-container">
+      <form className="register-form" onSubmit={handleSubmit}>
         <h2>Login</h2>
         <div className="form-control">
           <label htmlFor="email">Email:</label>
-          <input type="email" id="email" />
-        </div>
+          <input type="email" id="email" value={email} onChange={handleEmailChange} />
+        </div>  
         <div className="form-control">
           <label htmlFor="password">Password:</label>
-          <input type="password" id="password" />
+          <input type="password" id="password" value={password} onChange={handlePasswordChange} />
         </div>
         <button type="submit">Submit</button>
         <p><Link to="/register">¿Aún no tiene una cuenta? Cree una aquí</Link></p>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,26 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/register.css';
 import { Link } from 'react-router-dom';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:3000');
 
 function Register() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    const response = await fetch('/user/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+    socket.emit('newRegister', data); // Envia el mensaje de registro al servidor
+  };
+
   return (
     <div className="register-container">
-      <form className="register-form">
+      <form className="register-form" onSubmit={handleRegister}>
         <h2>Regístrese</h2>
         <div className="form-control">
           <label htmlFor="username">Nombre de usuario:</label>
-          <input type="text" id="username" />
+          <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
         </div>
         <div className="form-control">
           <label htmlFor="email">Correo electrónico:</label>
-          <input type="email" id="email" />
+          <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
         </div>
         <div className="form-control">
           <label htmlFor="password">Contraseña:</label>
-          <input type="password" id="password" />
+          <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
         </div>
         <button type="submit">Enviar</button>
-        <p><Link to="/login">¿Ya tiene una cuenta? Ingrese aquí</Link></p>
+        <p>
+          <Link to="/login">¿Ya tiene una cuenta? Ingrese aquí</Link>
+        </p>
       </form>
     </div>
   );
